3e3b1c843805a018a189a079dfaa991157cff002
Daniil Silin <daniil_silin@mail.ru>
2022-05-02 20:44:21 +0100

    Updated animations and width

--- components/main/mainCard.tsx
+++ components/main/mainCard.tsx
@@ -1,34 +1,55 @@
-import React, {ReactNode} from 'react';
-import NextLink from "next/link";
-import { BackgroundProps, Box, chakra, Heading, LayoutProps } from '@chakra-ui/react'
-import {isValidMotionProp, motion, useAnimation} from "framer-motion";
+import React, { ReactNode } from 'react'
+import NextLink from 'next/link'
+import {
+    BackgroundProps,
+    Box,
+    chakra,
+    Heading,
+    LayoutProps
+} from '@chakra-ui/react'
+import { isValidMotionProp, motion, useAnimation } from 'framer-motion'
 
 type MainCard = {
     children: ReactNode
-    bgColor: BackgroundProps['bgColor'],
-    url: string,
-    flex: number,
-    text: string,
-    minH: LayoutProps["minH"]
+    bgColor: BackgroundProps['bgColor']
+    url: string
+    flex: number
+    text: string
+    minH: LayoutProps['minH']
+    isActive?: boolean
 }
 
 const ChakraBox = chakra(motion.div, {
-    shouldForwardProp: (prop) => isValidMotionProp(prop) || prop === 'children',
-});
+    shouldForwardProp: prop => isValidMotionProp(prop) || prop === 'children'
+})
 
-const MainCard: React.FC<MainCard> = ({minH, text, url, bgColor, children, flex}) => {
+const MainCard: React.FC<MainCard> = ({
+    isActive = false,
+    minH,
+    text,
+    url,
+    bgColor,
+    children,
+    flex
+}) => {
+    const controls = useAnimation()
 
-    const controls = useAnimation();
+    const startAnimation = () =>
+        controls.start({
+            opacity: [0, 1],
+            x: ['0%', '-50%'],
+            transition: {
+                opacity: { duration: 1 },
+                duration: 10,
+                ease: 'linear',
+                repeat: Infinity,
+                repeatType: 'loop'
+            }
+        })
 
-    const startAnimation = () => controls.start({
-        x: ["0%", "-50%"],
-        transition: {
-            duration: 10,
-            ease: "linear",
-            repeat: Infinity,
-            repeatType: "loop",
-        }
-    });
+    if (isActive) {
+        startAnimation()
+    }
 
     return (
         <NextLink href={url} passHref>
@@ -36,37 +57,42 @@ const MainCard: React.FC<MainCard> = ({minH, text, url, bgColor, children, flex}
                 as={motion.div}
                 whileHover={{
                     scale: 0.9,
-                    transition: {duration: 0.8},
+                    transition: {
+                        duration: 0.8
+                    }
                 }}
-                onHoverStart={() => startAnimation()}
+                onHoverStart={() => !isActive && startAnimation()}
                 onHoverEnd={() => {
-                    controls.stop()
+                    !isActive && controls.stop()
                 }}
                 minH={minH}
                 flex={flex}
                 bgColor={bgColor}
                 p={5}
-                style={{"cursor": "pointer"}}
-                overflowX={"hidden"}
+                style={{
+                    cursor: 'pointer'
+                }}
+                overflow={'hidden'}
+                position={'relative'}
                 rounded={36}
             >
-                <Heading size={"lg"}>{children}</Heading>
+                <Heading size={'lg'}>{children}</Heading>
                 <Box
-                    position={"absolute"}
-                    top={"50%"}
-                    zIndex={-99}
+                    position={'absolute'}
+                    top={'50%'}
+                    zIndex={isActive ? 0 : -99}
                 >
                     <ChakraBox
-                        position={"relative"}
-                        whiteSpace={"nowrap"}
+                        position={'relative'}
+                        whiteSpace={'nowrap'}
                         animate={controls}
                     >
-                        <Heading size={"2xl"}>{text}</Heading>
+                        <Heading size={'2xl'}>{text}</Heading>
                     </ChakraBox>
                 </Box>
             </Box>
         </NextLink>
-    );
-};
+    )
+}
 
-export default MainCard;
+export default MainCard
--- components/main/products.tsx
+++ components/main/products.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import MainCard from "./mainCard";
+import React from 'react'
+import MainCard from './mainCard'
 
 const ProductsCard = () => {
     return (
         <MainCard
-            url={"/products"}
+            url={'/products'}
             flex={1}
-            bgColor={"purple.500"}
+            bgColor={'purple.500'}
             minH={380}
-            text={"Закуски к пенному, прохладительные напитки и многое другое. Закуски к пенному, прохладительные напитки и многое другое."}
+            isActive={true}
+            text={
+                'Закуски к пенному, прохладительные напитки и многое другое. Закуски к пенному, прохладительные напитки и многое другое.'
+            }
         >
             Просмотреть ассортимент
         </MainCard>
-    );
-};
+    )
+}
 
-export default ProductsCard;
+export default ProductsCard
