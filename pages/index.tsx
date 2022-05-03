a2d5d26c7095fee2ec43866c3fb18202f9526950
Daniil Silin <daniil_silin@mail.ru>
2022-05-03 02:44:15 +0100

    Updated main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,46 @@
-import type {NextPage} from 'next'
+import type { NextPage } from 'next'
 import Head from 'next/head'
-import {Box, Flex, Heading} from "@chakra-ui/react";
-import ProductsCard from "../components/main/products";
-import AboutCard from "../components/main/about";
-import ContactsCard from "../components/main/contacts";
-import NewsCard from "../components/main/news";
+import { Box, Flex, Heading } from '@chakra-ui/react'
+import ProductsCard from '../components/main/products'
+import AboutCard from '../components/main/about'
+import ContactsCard from '../components/main/contacts'
+import NewsCard from '../components/main/news'
 
 const Home: NextPage = () => {
     return (
-        <Box
-            bgColor={"black"}>
+        <Box bgColor={'black'}>
             <Head>
                 <title>Старый Причал</title>
-                <meta name="description" content="Магазин напитков и закусок к пенному"/>
-                <link rel="icon" href="/favicon.ico"/>
+                <meta
+                    name="description"
+                    content="Магазин напитков и закусок к пенному"
+                />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <Box as={"main"}>
-                <Flex minH={"280px"} h={"40vh"} justifyContent={"center"} align={"center"}>
-                    <Heading as={"h1"}>
-                        Старый Причал
-                    </Heading>
+            <Box as={'main'}>
+                <Flex
+                    minH={'280px'}
+                    h={'40vh'}
+                    justifyContent={'center'}
+                    align={'center'}
+                >
+                    <Heading as={'h1'}>Старый Причал</Heading>
                 </Flex>
 
-                <Flex minHeight={{base: "60vh"}} gap={2} p={4} direction={{base: "column", lg: "row"}}>
-                    <ProductsCard/>
-                    <Flex direction={"column"} flex={1} gap={2}>
+                <Flex
+                    minHeight={{ base: '60vh' }}
+                    gap={2}
+                    p={4}
+                    direction={{ base: 'column', lg: 'row' }}
+                >
+                    <ProductsCard />
+                    <Flex direction={'column'} flex={1} gap={2}>
                         <Flex gap={2} flex={1}>
-                            <AboutCard/>
-                            <NewsCard/>
+                            <AboutCard />
+                            <NewsCard />
                         </Flex>
-                        <ContactsCard/>
+                        <ContactsCard />
                     </Flex>
                 </Flex>
             </Box>
